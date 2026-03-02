d19cce535284b9a2a3c327659187843950c57ce0
Dancun Ochieng <duncanochieng48@gmail.com>
2026-03-02 23:14:53 +0300

    second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,15 @@ import PositioningStrip from '../components/Home/PositioningStrip';
 import ImageGrid from '../components/Home/ImageGrid';
 import WellnessPrograms from '../components/Home/WellnessPrograms';
 import PodcastSection from '../components/Home/PodcastSection';
+import { Helmet } from 'react-helmet-async';
 
 const HomePage = () => {
   return (
+    <>
+    <Helmet>
+      <title>The Catalog - Home</title>
+      <meta name="description" content="Discover The Catalog, your go-to resource for curated wellness solutions designed to optimize performance and enhance wellbeing. Explore our expert insights, product reviews, and transformative programs to elevate your health and productivity." />
+    </Helmet>
     <main className="pt-16 md:pt-20">
       <Hero />
       <PositioningStrip />
@@ -14,6 +20,7 @@ const HomePage = () => {
       <WellnessPrograms />
       <PodcastSection />
     </main>
+    </>
   );
 };
 
